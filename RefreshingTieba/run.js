a008f83f5807ba47039fd10d2050c2967c72bb12
8qwe24657913 <1197499769@qq.com>
2017-01-17 12:46:41 +0800

    把配置文件独立出来并分行写，方便diff

--- RefreshingTieba/run.js
+++ RefreshingTieba/run.js
@@ -1,46 +1,61 @@
-function inject() {
+/*jshint esversion: 6 */
+function inject(setting, getSpecialModules) {
     'use strict';
-    var debugMode = true;
+    var {
+        debugMode,
+        sensitiveWords,
+        selector,
+        bigpipeWhiteList,
+        bigpipeBlackList,
+        moduleWhiteList,
+        moduleBlackList,
+    } = setting;
 
     function createNoop() {
-        return function() {}
-    };
+        return function() {};
+    }
     var noop = createNoop();
 
     function emptyStr() {
-        return ''
-    };
-    if (Object.freeze) Object.freeze(noop), Object.freeze(emptyStr);
+        return '';
+    }
+    if (Object.freeze) {
+        Object.freeze(noop);
+        Object.freeze(emptyStr);
+    }
+    var specialModules = getSpecialModules(noop, emptyStr);
     // Fxck PercentLoaded
     Object.defineProperties(HTMLEmbedElement.prototype, {
         PercentLoaded: {
             configurable: true,
             enumerable: false,
-            value: () => 100,
+            value() {
+                return 100;
+            },
             writable: true
         },
         getData: {
             configurable: true,
             enumerable: false,
-            value: () => {},
+            value: noop,
             writable: true
         },
         setData: {
             configurable: true,
             enumerable: false,
-            value: () => {},
+            value: noop,
             writable: true
         }
     });
     // Logging
-    var log = noop;
+    var log;
     console.info('清爽贴吧正在运行中');
     if (debugMode) {
         var arr = [],
             arr2 = [];
         log = function(txt) {
-            arr.push(txt)
-        }
+            arr.push(txt);
+        };
         console.info('被过滤的模块:', arr);
         console.info('被放行的模块:', arr2);
     }
@@ -50,21 +65,23 @@ function inject() {
         if (prop) {
             var newProp = filter(prop);
             if (newProp && newProp !== prop) parent[name] = newProp;
-        } else {
-            Object.defineProperty(parent, name, {
-                configurable: true,
-                enumerable: true,
-                get: noop,
-                set: function(e) {
-                    Object.defineProperty(parent, name, {
-                        configurable: true,
-                        enumerable: true,
-                        writable: true,
-                        value: filter(e) || e
-                    });
-                }
-            });
+            return;
         }
+        var descriptor = Object.getOwnPropertyDescriptor(parent, name);
+        var enumerable = descriptor ? descriptor.enumerable : true;
+        Object.defineProperty(parent, name, {
+            configurable: true,
+            enumerable,
+            get: noop,
+            set: function(e) {
+                Object.defineProperty(parent, name, {
+                    configurable: true,
+                    enumerable,
+                    writable: true,
+                    value: filter(e) || e
+                });
+            }
+        });
     }
 
     function hijack(parent, path, filter) {
@@ -76,9 +93,6 @@ function inject() {
             hijackOnce(node, name[pos++], f);
         }(parent));
     }
-    // what to filter
-    var sensitiveWords = ['encourage', 'entertainment', 'hottopic', 'firework', 'adsense', 'dasense', 'stat', 'money', 'game', 'tcharge', 'vip', 'tdou', 'celebrity', 'tbmall', 'wallet', 'cashier', 'qianbao', 'purchase', 'track', 'stock', 'promoter', 'pay', 'u9aside', 'duoku', 'xiu8', 'meizhi', 'adpost', 'advertise', 'recommend', 'props', 'snowflow', 'saveface', 'medal', 'pkfixedbubble', 'locality', 'topicrank', 'force_login', 'urlcheck', 'share', 'flashlcs', 'icons', 'asidead', 'platformweal', 'headrecom', 'spreadad', 'asidead', 'score', 'icon', 'localpbtop', 'fakegif', 'localposter', 'interaction', 'comtrial', 'cpro', 'pc2client', 'tenyears', 'pad_overlay', 'spageliveshowslide', 'aside_platform', 'avideo', 'tb_gram', 'live_tv'];
-    var whiteList = ['album/component/initApiConfig'];
 
     function hasSensitiveWords(text) {
         text = text.toLowerCase();
@@ -89,39 +103,36 @@ function inject() {
     // Bigpipe
     hijack(window, 'Bigpipe', function(Bigpipe) {
         // 加载过滤
-        var names = ['frs-aside/pagelet/ad'];
-        var _register = Bigpipe.register;
-
         function check(name) { // 放行返回true
-            return !hasSensitiveWords(name) || whiteList.includes(name);
-        }
-
-        function loadingFilter(name) {
-            return check(name) || (log('Blocked loading: ' + name), false);
-        }
-        Bigpipe.register = function(name, info) {
-            if (names.includes(name) || !check(name)) {
-                log('Blocked loading: ' + name);
-                return {
-                    then: noop
-                };
+            if (bigpipeBlackList.includes(name) || hasSensitiveWords(name) && !bigpipeWhiteList.includes(name)) {
+                if (debugMode) log('Blocked loading: ' + name);
+                return false;
             }
-            if (info.scripts) info.scripts = info.scripts.filter(loadingFilter);
-            if (info.styles) info.styles = info.styles.filter(loadingFilter);
+            return true;
+        }
+        var _register = Bigpipe.register;
+        Bigpipe.register = function(name, info) {
+            if (!check(name)) return {
+                then: noop
+            };
+            if (info.scripts) info.scripts = info.scripts.filter(check);
+            if (info.styles) info.styles = info.styles.filter(check);
             return _register.call(Bigpipe, name, info);
         };
         // 模板过滤
-        var selector = '[id="pagelet_frs-aside/pagelet/ad"],[id*="encourage"],[id*="entertainment"],[id*="hottopic"],[id*="firework"],[class*="encourage"],[class*="entertainment"],[class*="firework"]';
+        function remove(elem) {
+            elem.remove();
+        }
 
         function elementFilter(html) {
             var doc = new DOMParser().parseFromString(html, 'text/html');
-            [].forEach.call(doc.querySelectorAll(selector), function(elem) {
-                var noscript = doc.createElement('noscript');
-                noscript.id = elem.id;
-                noscript.className = elem.className;
-                elem.parentNode.replaceChild(noscript, elem);
-                log('Blocked element: ' + ('id' in elem ? ('id="' + elem.id + '"') : ('class="' + elem.className + '"')));
-            });
+            [].forEach.call(doc.querySelectorAll(selector), debugMode ? function(elem) {
+                var id, className;
+                if (elem.hasAttribute('id')) id = elem.id;
+                if (elem.hasAttribute('class')) className = elem.className;
+                remove(elem);
+                log('Blocked element: ' + (id ? `id="${id}"` : `class="${className}"`));
+            } : remove);
             return doc.body.innerHTML;
         }
         Bigpipe.debug(true);
@@ -137,11 +148,9 @@ function inject() {
     });
     // 模块过滤
     hijack(window, '_.Module', function(Module) {
-        var blackList = ['ueditor/widget/topic_suggestion', 'puser/widget/myApp', 'fanclub/widget/fancard'];
-
         function check(module) { // 放行返回true
-            if (blackList.includes(module) || hasSensitiveWords(module) && !whiteList.includes(module)) {
-                log('Blocked module: ' + module);
+            if (moduleBlackList.includes(module) || hasSensitiveWords(module) && !moduleWhiteList.includes(module)) {
+                if (debugMode) log('Blocked module: ' + module);
                 return false;
             }
             return true;
@@ -150,57 +159,6 @@ function inject() {
         Module.use = function(a, b, c, d) {
             if (!check(a)) return;
             return _use.call(Module, a, b, c, d);
-        }
-        var specialModules = {
-            "props/component/PropsApi": {
-                showUIHtml: emptyStr
-            },
-            "puser/component/PropsApi": {
-                showUIHtml: emptyStr
-            },
-            "props/component/PropsApi": {
-                showUI: emptyStr,
-                showUIHtml: emptyStr
-            },
-            "user/widget/icons": {
-                getPreIconHtml: emptyStr,
-                getTbvipIconHtml: emptyStr,
-                getIconsHtml: emptyStr
-            },
-            "puser/widget/icons": {
-                getPreIconHtml: emptyStr,
-                getTbvipIconHtml: emptyStr,
-                getIconsHtml: emptyStr
-            },
-            "props/widget/Residual": {
-                showUI: emptyStr
-            },
-            "user/widget/month_icon": {
-                getMonthIcon: emptyStr
-            },
-            "puser/widget/MonthIcon": {
-                getMonthIcon: emptyStr
-            },
-            "ihome/widget/MonthIcon": {
-                getMonthIcon: emptyStr
-            },
-            "user/widget/interaction": {
-                _resetDataObj: noop
-            },
-            "puser/widget/Interaction": {
-                _resetDataObj: noop
-            },
-            "ihome/widget/Interaction": {
-                _resetDataObj: noop
-            },
-            "tbui/widget/tbshare_popup": {
-                setShareContent: noop
-            },
-            "tbmall/component/util": {
-                getMaxLevel: function() {
-                    return 0
-                }
-            },
         };
         var defined = {};
         var createDebugInitial;
@@ -213,11 +171,11 @@ function inject() {
                         console.warn('Undefined property:', path, property, defined[path], target);
                         return function() {
                             //debugger;
-                        }
+                        };
                     }
                 }));
-            }
-        }
+            };
+        };
 
         function fakeDefine(path, sub_ori) {
             if (defined[path]) {
@@ -235,12 +193,6 @@ function inject() {
             });
         }
 
-        function requireInstanceAsync(path) {
-            if (!check(path)) return fakeDefine(path);
-            this.__checkUse(path);
-            Module.use.apply(null, arguments);
-        }
-
         function moduleFilter(path) {
             return check(path) || (fakeDefine(path), false);
         }
@@ -253,17 +205,16 @@ function inject() {
                 } else if ('string' === typeof info.requires) {
                     if (!check(info.requires)) {
                         fakeDefine(info.requires);
-                        delete info.requires;
+                        info.requires = undefined;
                     }
                 } else {
                     console.warn('贴吧精简脚本遇到问题：未知的requires字段', info);
                 }
-            };
-            if (info.sub) info.sub.requireInstanceAsync = requireInstanceAsync;
+            }
             defined[info.path] = 1;
             if (debugMode) arr2.push(info.path);
             return _define.call(Module, info);
-        }
+        };
     });
     // 统计过滤
     function setNoop(parent, name) {
@@ -291,18 +242,19 @@ function inject() {
         document.addEventListener('DOMContentLoaded', function listener() { // DOM加载完成后改成已登录状态
             is_login = 1;
             document.removeEventListener('DOMContentLoaded', listener, true);
-        }, true)
+        }, true);
         Object.defineProperty(user, 'is_login', { // 不直接改，因为会被Object.freeze()，换成getter（虽然PB页现在不freeze
             configurable: false,
             get: function() {
-                return is_login
+                return is_login;
             }
         });
     });
 }
 (function() {
+    'use strict';
     var s = document.createElement('script');
-    s.textContent = '(' + inject.toString() + '())';
+    s.textContent = `(${inject}(${JSON.stringify(setting)},${getSpecialModules}))`;
     document.documentElement.appendChild(s);
     s.remove();
-}())
+}());
---
+++ RefreshingTieba/setting.js
@@ -0,0 +1,170 @@
+/*jshint esversion: 6 */
+// 是否开启debug模式
+var debugMode = false;
+// 通用模糊匹配，包含这个词就屏蔽
+var sensitiveWords = split(`
+encourage
+entertainment
+hottopic
+firework
+adsense
+dasense
+stat
+money
+game
+tcharge
+vip
+tdou
+celebrity
+tbmall
+wallet
+cashier
+qianbao
+purchase
+track
+stock
+promoter
+pay
+u9aside
+duoku
+xiu8
+meizhi
+adpost
+advertise
+recommend
+props
+snowflow
+saveface
+medal
+pkfixedbubble
+locality
+topicrank
+force_login
+urlcheck
+share
+flashlcs
+icons
+asidead
+platformweal
+headrecom
+spreadad
+asidead
+score
+icon
+localpbtop
+fakegif
+localposter
+interaction
+comtrial
+cpro
+pc2client
+tenyears
+pad_overlay
+spageliveshowslide
+aside_platform
+avideo
+tb_gram
+live_tv
+`);
+// 从模板中移除的元素
+var selector = join(`
+[id="pagelet_frs-aside/pagelet/ad"]
+[id*="encourage"]
+[id*="entertainment"]
+[id*="hottopic"]
+[id*="firework"]
+[class*="encourage"]
+[class*="entertainment"]
+[class*="firework"]
+.game-head-game-info-wrapper
+`);
+// bigpipe黑名单，全名
+var bigpipeBlackList = split(`
+frs-aside/pagelet/ad
+`);
+// bigpipe白名单，全名
+var bigpipeWhiteList = split(`
+`);
+// module白名单，全名
+var moduleWhiteList = split(`
+album/component/initApiConfig
+`);
+// module黑名单，全名
+var moduleBlackList = split(`
+ueditor/widget/topic_suggestion
+puser/widget/myApp
+fanclub/widget/fancard
+`);
+// 屏蔽后需要覆盖原方法的模块
+function getSpecialModules(noop, emptyStr) {
+    'use strict';
+    return {
+        "props/component/PropsApi": {
+            showUI: emptyStr,
+            showUIHtml: emptyStr
+        },
+        "puser/component/PropsApi": {
+            showUIHtml: emptyStr
+        },
+        "user/widget/icons": {
+            getPreIconHtml: emptyStr,
+            getTbvipIconHtml: emptyStr,
+            getIconsHtml: emptyStr
+        },
+        "puser/widget/icons": {
+            getPreIconHtml: emptyStr,
+            getTbvipIconHtml: emptyStr,
+            getIconsHtml: emptyStr
+        },
+        "user/widget/month_icon": {
+            getMonthIcon: emptyStr
+        },
+        "puser/widget/MonthIcon": {
+            getMonthIcon: emptyStr
+        },
+        "ihome/widget/MonthIcon": {
+            getMonthIcon: emptyStr
+        },
+        "user/widget/interaction": {
+            _resetDataObj: noop
+        },
+        "puser/widget/Interaction": {
+            _resetDataObj: noop
+        },
+        "ihome/widget/Interaction": {
+            _resetDataObj: noop
+        },
+        "props/widget/Residual": {
+            showUI: emptyStr
+        },
+        "tbui/widget/tbshare_popup": {
+            setShareContent: noop
+        },
+        "tbmall/component/util": {
+            getMaxLevel: function() {
+                return 0;
+            }
+        },
+    };
+}
+var setting = {
+    debugMode,
+    sensitiveWords,
+    selector,
+    bigpipeWhiteList,
+    bigpipeBlackList,
+    moduleWhiteList,
+    moduleBlackList,
+};
+
+function orig(wtf) {
+    return wtf;
+}
+
+function split(str) {
+    return str.split('\n').filter(orig);
+}
+
+function join(str) {
+    return split(str).join();
+}
